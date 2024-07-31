e80af3a89a00793e7ba5addf245d54c93ce96a91
RGalindo2004 <151685550+RGalindo2004@users.noreply.github.com>
2024-07-31 12:47:44 -0600

    Prueba 3
    Prueba 3

--- vite.config.js
+++ vite.config.js
@@ -7,46 +7,47 @@ import handlebars from 'vite-plugin-handlebars';
 import getPageContext from './data';
 
 export default defineConfig({
-  appType: "mpa",
-  base: "/portafolioactualizado/",
-  build: {
-    rollupOptions: {
-      input: Object.fromEntries(
-        [
-          ...glob.sync('./!(dist)/*.html').map(
-            file => [
-              file.slice(0, file.length - path.extname(file).length),
-              resolve(__dirname, file)
-            ]
-          ),
-          ...glob.sync('./*.html').map(
-            file => [
-              file.slice(0, file.length - path.extname(file).length),
-              resolve(__dirname, file)
-            ]
-          )
-        ]
-      )
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
+    appType: "mpa",
+    base: "/portafolioactualizado/",
+    build: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                [
+                    ...glob.sync('./!(dist)/*.html').map(
+                        file => [
+                            file.slice(0, file.length - path.extname(file).length),
+                            resolve(__dirname, file)
+                        ]
+                    ),
+                    ...glob.sync('./*.html').map(
+                        file => [
+                            file.slice(0, file.length - path.extname(file).length),
+                            resolve(__dirname, file)
+                        ]
+                    )
+                ]
+            )
+        }
     },
-  },
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, 'parciales'),
-      context: (pagePath) => {
-        console.log(pagePath);
-        const contextVariable = getPageContext(pagePath);
-        console.log(contextVariable);
-        return contextVariable;
-      }
-    }),
-    htmlPurge({}),
-    ViteMinifyPlugin()
-  ]
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+            },
+        },
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'parciales'),
+            context: (pagePath) => {
+                console.log(pagePath);
+                const contextVariable = getPageContext(pagePath);
+                console.log(contextVariable);
+                return contextVariable;
+            }
+        }),
+
+        htmlPurge({}),
+        ViteMinifyPlugin()
+    ]
 });
