3289519670537b897fcbcb97ab1f61d51cd8a715
RGalindo2004 <151685550+RGalindo2004@users.noreply.github.com>
2024-07-31 12:44:09 -0600

    Prueba2
    Prueba2

--- vite.config.js
+++ vite.config.js
@@ -6,42 +6,47 @@ import path, { resolve } from "node:path";
 import handlebars from 'vite-plugin-handlebars';
 import getPageContext from './data';
 
-export default defineConfig(
-    {
-        appType: "mpa",
-        base: "/portafolioactualizado/",
-        build: {
-            rollupOptions: {
-                input: Object.fromEntries(
-                    [
-                        ...glob.sync('./!(dist)/*.html').map(
-                            file=> [
-                                file.slice(0, file.length - path.extname(file).length),
-                                resolve(__dirname, file)
-                            ]
-                        ),
-                        ...glob.sync('./*.html').map(
-                            file=> [
-                                file.slice(0, file.length - path.extname(file).length),
-                                resolve(__dirname, file)
-                            ]
-                        )
-                    ]
-                )
-            }
-        },
-        plugins: [
-            handlebars({
-                partialDirectory: resolve(__dirname, 'parciales'),
-                context: (pagePath)=>{
-                    console.log(pagePath);
-                    const contextVariable = getPageContext(pagePath);
-                    console.log(contextVariable);
-                    return contextVariable;
-                }
-            }),
-            htmlPurge({}),
-            ViteMinifyPlugin()
+export default defineConfig({
+  appType: "mpa",
+  base: "/portafolioactualizado/",
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        [
+          ...glob.sync('./!(dist)/*.html').map(
+            file => [
+              file.slice(0, file.length - path.extname(file).length),
+              resolve(__dirname, file)
+            ]
+          ),
+          ...glob.sync('./*.html').map(
+            file => [
+              file.slice(0, file.length - path.extname(file).length),
+              resolve(__dirname, file)
+            ]
+          )
         ]
+      )
     }
-);
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'parciales'),
+      context: (pagePath) => {
+        console.log(pagePath);
+        const contextVariable = getPageContext(pagePath);
+        console.log(contextVariable);
+        return contextVariable;
+      }
+    }),
+    htmlPurge({}),
+    ViteMinifyPlugin()
+  ]
+});
